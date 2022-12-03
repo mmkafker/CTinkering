1650e7f4f3034bb2f26603716b3f1f6246d7f991
mkafker <kafkem@uw.edu>
2022-12-03 12:12:50 -0800

    Hello world, and basic list progamming.

---
+++ sumlist/sumlist.c
@@ -0,0 +1,17 @@
+// Create a list of numbers up to N. Sum them.
+
+#include <stdio.h>
+ 
+int main()
+{
+    int N = 100000;
+    int temp[N];
+    long int tot; tot = 0;
+    int i;
+    for (i=0;i<N;i++) temp[i] = i;
+    for (i=0;i<N;i++) tot +=temp[i];
+    printf("tot = %ld\n",tot);
+
+  
+    return 0;
+}
