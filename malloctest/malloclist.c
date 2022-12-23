21dd185d6591d7016df22fe5d99e97f8d114c719
mkafker <kafkem@uw.edu>
2022-12-23 13:36:31 -0800

    Added explicit malloc syntax.

--- malloctest/malloclist.c
+++ malloctest/malloclist.c
@@ -1,16 +1,18 @@
+// Create a list of numbers up to N. Sum 1/(n+1).
 
 #include <stdio.h>
- 
+#include <stdlib.h>
+
 int main()
 {
-    int N = 100000;
-    int temp[N];
-    long int tot; tot = 0;
-    int i;
+    long int N = 100000000;
+    long int *temp;
+    temp = malloc(N*sizeof(long int));
+    double tot; tot = 0.0;
+    long int i;
     for (i=0;i<N;i++) temp[i] = i;
-    for (i=0;i<N;i++) tot +=temp[i];
-    printf("tot = %ld\n",tot);
-
-  
+    for (i=0;i<N;i++) tot +=1.0/(temp[i]+1.0);
+    printf("tot = %.17g\n",tot);
+    free(temp);
     return 0;
 }
